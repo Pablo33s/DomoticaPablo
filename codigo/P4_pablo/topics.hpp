942054a774806df389bf7cfe5646198b886fc36c
Pablo33s <pablonuro15112006@gmail.com>
2024-06-05 12:17:27 +0200

    Add files via upload

---
+++ codigo/P4_pablo/topics.hpp
@@ -0,0 +1,5 @@
+
+const char* temperatura_topic = "casa/sensores/temperatura6";
+const char* humidade_topic = "casa/sensores/humidade6";
+const char* depuracion_topic = "depuracion6";  
+const char* rele_topic = "casa/actuadores/rele6";  
